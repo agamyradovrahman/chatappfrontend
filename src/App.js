873c73737d569ace39b93f00bcf85657a3f4b591
rahman <rahmanagamyradov1111@gmail.com>
2023-05-02 06:44:49 +0300

    update12

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Register from "./pages/Register/Register";
 import Login from "./pages/login/Login";
+import { LoginUser } from "./actions/userauth";
+
+
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(async () => {
+    const dataaa = await JSON.parse(
+      localStorage.getItem("chatapp")
+    );
+
+    if(dataaa){
+      console.log(dataaa)
+      dispatch(LoginUser(dataaa))
+    } else {
+      console.log("user is not exist");
+    }
+  }, []);
+
 
 
   return (
+
+    
     
     <BrowserRouter >
       <Routes>
